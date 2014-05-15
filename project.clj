72d52aaf2c36d44628120ab7dec426f78cf7d417
Arnab Deka <arnab.deka@gmail.com>
2014-05-15 16:15:16 +0530

    setup dependencies etc.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject homebrew-dependency-visualizer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "set of tools to homebrew dependency visualize"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :license {:name "MIT License"
+            :url "https://github.com/arnab/homebrew-dependency-visualizer/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.8"]
+                                  [print-foo "0.5.0"]
+                                  [lein-kibit "0.0.8"]
+                                  [jonase/eastwood "0.1.2"]]}} )
