8df5a3cfc02c561a8b0d2deddba8ccc841747a3f
Arnab Deka <arnab.deka@gmail.com>
2014-05-15 16:15:32 +0530

    find brew executible.

--- src/homebrew_dependency_visualizer/core.clj
+++ src/homebrew_dependency_visualizer/core.clj
@@ -1,6 +1,21 @@
-(ns homebrew-dependency-visualizer.core)
+(ns homebrew-dependency-visualizer.core
+  (:require [clojure.java.shell :as shell :only [sh]]
+            [clojure.string :as str :only [join trim-newline]])
+  (:import java.io.IOException))
+(use '[print.foo])
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn brew-executible
+  "find the absolute location of the brew executible"
+  []
+  (let [cmd ["which" "brew"]]
+    (let [{:keys [exit out err]} (apply shell/sh cmd)]
+      (if (zero? exit)
+        (str/trim-newline out)
+        (throw (IOException. (str "Couldn't find brew using '"
+                                  (str/join " " cmd)
+                                  "'. Exit: " exit ", Error:" err)))))))
--- test/homebrew_dependency_visualizer/core_test.clj
+++ test/homebrew_dependency_visualizer/core_test.clj
@@ -1,7 +1,3 @@
 (ns homebrew-dependency-visualizer.core-test
   (:require [clojure.test :refer :all]
             [homebrew-dependency-visualizer.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
