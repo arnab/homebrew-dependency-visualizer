dd644237a548bcc38b44deed0b6e7fe3417ef1c2
Arnab Deka <arnab.deka@gmail.com>
2014-05-15 16:49:19 +0530

    list homebrew packages.

--- src/homebrew_dependency_visualizer/core.clj
+++ src/homebrew_dependency_visualizer/core.clj
@@ -1,8 +1,19 @@
 (ns homebrew-dependency-visualizer.core
-  (:require [homebrew-dependency-visualizer.shell :as shell]))
+  (:require [homebrew-dependency-visualizer.shell :as shell]
+            [clojure.string :as str only [split]]))
 
 (defn brew-executible
   "find the absolute location of the brew executible"
   []
   (shell/exec ["which" "brew"]
               "find homebrew"))
+
+(defn list-packages
+  "returns a list of homebrew packages foubd on the system"
+  []
+  (let [msg "list installed homebrew packages"
+        brew (brew-executible)
+        pkgs (shell/exec [brew "list"] msg)]
+    (str/split pkgs #"\n")))
+
+;# (list-packages)
